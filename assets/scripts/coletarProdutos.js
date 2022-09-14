fd7a49db8b89ed1a0bef510d35f01aa594ce35e6
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-09-14 15:13:10 -0300

    criando o botao

--- assets/scripts/coletarProdutos.js
+++ assets/scripts/coletarProdutos.js
@@ -1,17 +1,20 @@
 const res = document.getElementById('res')
 
-for(let c = 1; c < 899; c++){
+for(let c = 1; c < 150; c++){
     const p = document.createElement('p')
     const span = document.createElement('span')
     const div = document.createElement('div')
+    const btn = document.createElement('button')
     div.className = 'card'
     const img = document.createElement('img')
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${c}.png`
     img.id = c
     p.innerHTML += `Pokemon ${c}`
     span.innerHTML += `R$ 100,00`
+    btn.innerHTML += 'Comprar'
     div.appendChild(img)
     div.appendChild(p)
     div.appendChild(span)
+    div.appendChild(btn)
     res.appendChild(div)
 }
