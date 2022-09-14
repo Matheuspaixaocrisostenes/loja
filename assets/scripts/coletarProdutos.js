c1c2c4e61325d7763dddfc8c69f7fb68c151b09b
Matheuspaixaocrisostenes <matheuspcrisostenes@gmail.com>
2022-09-14 15:05:13 -0300

    adicionando preco

--- assets/scripts/coletarProdutos.js
+++ assets/scripts/coletarProdutos.js
@@ -2,13 +2,16 @@ const res = document.getElementById('res')
 
 for(let c = 1; c < 899; c++){
     const p = document.createElement('p')
+    const span = document.createElement('span')
     const div = document.createElement('div')
     div.className = 'card'
     const img = document.createElement('img')
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${c}.png`
     img.id = c
     p.innerHTML += `Pokemon ${c}`
+    span.innerHTML += `R$ 100,00`
     div.appendChild(img)
     div.appendChild(p)
+    div.appendChild(span)
     res.appendChild(div)
 }
